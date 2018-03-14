33102a2dd6cd6c5b3ebd162d77656bedd0d5ff81
Keruba <37382082+Keruba@users.noreply.github.com>
2018-03-14 17:21:21 -0300

    Update Ejercicio1.m

--- Ejercicio1.m
+++ Ejercicio1.m
@@ -1,5 +1,5 @@
 A=1 ; B=9 ; C=1 ; D=8 ; E=4 ; F=7 ; G=2 ; H=4 ; I=5;
-%% mi rut es AB.CDE.FGH-I
+%% mi rut es AB.CDE.FGHI
 t=0 : 0.3 : 30; v=exp(-((B+A)/(2*C*(D+E)))*t).*sin(t);
 subplot 121; plot(t,v):title('PLOT');
 xlabel('tiempo [seg]'); ylabel('Volts'); grid;
